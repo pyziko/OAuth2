e13d4a2f0da1abcaaea52fa0384201218ca1ebd6
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-06-14 14:52:34 +0100

    first commit

---
+++ oa.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import g
+from flask_oauthlib.client import OAuth
+
+oauth = OAuth()
+
+# todo info if Oauth 1, request_token_url is not None
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.getenv("GITHUB_CONSUMER_KEY"),
+    consumer_secret=os.getenv("GITHUB_CONSUMER_SECRET"),
+    request_token_params={"scope": "email"},
+    base_url="https://api.github.com/",
+    request_token_url=None,
+    access_token_method="POST",
+    access_token_url="https://github.com/login/oauth/access_token",
+    authorize_url="https://github.com/login/oauth/authorize"
+)
+
+
+@github.tokengetter
+def get_github_token():
+    if "access_token" in g:
+        return g.access_token
---
+++ resources/github_login.py
@@ -0,0 +1,44 @@
+from flask import g, request
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource
+
+from models.user import UserModel
+from oa import github
+
+
+# todo info same for facebook, google, twitter etc :::: parameters such as where user is stored might be different
+
+class GithubLogin(Resource):
+    @classmethod
+    def get(cls):
+        return github.authorize(callback="http://localhost:5000/login/github/authorized")  # todo info same as on github
+
+
+class GithubAuthorize(Resource):
+    @classmethod
+    def get(cls):
+        response = github.authorized_response()
+
+        if response is None or response.get("access_token") is None:
+            error_response= {
+                "error": request.args["error"],
+                "error_description": request.args["error_description"]
+            }
+            return error_response
+
+        g.access_token = response["access_token"]
+        # todo info note user can change user name hence u might validate email too
+        # todo info we can also pass token param to the arg, but cleaner code as done in oa @github.tokengetter
+        github_user = github.get("user")
+        github_username = github_user.data["login"]
+
+        user = UserModel.find_by_username(github_username)
+
+        if not user:
+            user = UserModel(username=github_username, password=None)
+            user.save_to_db()
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_access_token(user.id)
+
+        return {"access_token": access_token, "refresh_token": refresh_token}, 200
